52785d5d8bd128563e1494e4f8599a62cc27a95e
“devSync52” <dev@synccomusa.com>
2025-05-04 14:19:48 -0400

    remove backup folder to prevent build errors

--- src_backup/lib/supabase-server.ts
+++
@@ -1,8 +0,0 @@
-import { cookies } from 'next/headers'
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-
-export const createServerSupabaseClient = () => {
-  const cookieStore = cookies()
-
-  return createServerComponentClient({ cookies: () => cookieStore })
-}
