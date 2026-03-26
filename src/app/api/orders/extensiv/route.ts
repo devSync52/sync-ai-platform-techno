628f2d1c8a17e3b614cdd472b66ede53b65787f8
sujantechnoexponent <sujanbag@technoexponent.com>
2026-03-26 14:57:42 +0530

    fixed

--- src/app/api/orders/extensiv/route.ts
+++ src/app/api/orders/extensiv/route.ts
@@ -397,7 +397,7 @@ export async function POST(req: NextRequest) {
       Accept: "application/json",
       "Content-Type": "application/json",
     };
-
+    //test data for facility lookup
     const fetchDefaultFacilityId = async (): Promise<{
       id: number | null;
       debug?: any;
